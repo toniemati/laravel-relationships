53cb3fa83960bff6402c011aac7b4a807e75e238
toniemati <matikas2000@gmail.com>
2020-12-01 19:41:07 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $user = User::factory()->create();
+
+    $randomPhone = rand(100, 999) . '-' . rand(100, 999) . '-' . rand(100, 999);
+
+    $user->phone()->create([
+        'phone' => $randomPhone
+    ]);
+});
